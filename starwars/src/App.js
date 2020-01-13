788202f20073f5f2e5b8819f4ee70000e8f4281f
William Schwindt <william@Williams-MacBook-Pro.local>
2020-01-12 16:56:22 -0800

    data fetched and person component rendered

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
+import PersonSlide from './components/PersonSlide';
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+  const [person, setPerson] = useState([])
 
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+ useEffect(() => {
+   axios
+   .get("https://swapi.co/api/people")
+
+   .then((res) => {
+     console.log(res);
+     
+   })
+
+   .catch((err) => {
+     console.log(err);
+   })
+ }, [])
 
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      <PersonSlide />
     </div>
   );
 }
---
+++ starwars/src/components/PersonSlide.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function PersonSlide(props) {
+    return(
+        <div>
+            
+        </div>
+    )
+}
+
+export default PersonSlide;
